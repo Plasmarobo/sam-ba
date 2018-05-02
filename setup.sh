535b01c268403950edf80ad2ad7cccbbad937cb8
Austen Higgins-Cassidy <austen@thelevelup.com>
2018-05-02 16:25:25 -0400

    Automated setup script

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+. ./env.sh
+
+apt-get install -y git-core build-essential qt5-default libqt5serialport5-dev qtdeclarative5-dev libqt5qml5 libqt5quick5 build-essential qtbase5-dev-tools qttools5-dev-tools
+
+mkdir -p $QTRPI_DIR
+pushd $QTRPI_DIR
+git clone https://github.com/neuronalmotion/qtrpi.git $QTRPI_DIR
+./init-qtrpi-minimal.sh
+./deploy-qtripi.sh --prepare-rpi
+popd
+
+mkdir -p $BUILD_DIR
+mkdir -p $RELEASE_DIR
+
+pushd $BUILD_DIR
+$CROSS_COMPILER -r $DIR/sam-ba.pro
+make INSTALL_ROOT=$RELEASE_DIR install
+popd
+
+cp ./flash_dory.qml $RELEASE_DIR/flash_dory.qml
+
+rsync -rv $RELEASE_DIR/ $PI_USERNAME@$PI_HOSTNAME:$PI_INSTALL_DIR
+
